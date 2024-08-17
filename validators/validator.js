8cc80cac3442cb85f7dd6de8719cd4f3737475b7
kmkmadusanka <kmkasunmadushanka@gmail.com>
2024-08-17 17:44:50 +1000

    update validator

--- validators/validator.js
+++ validators/validator.js
@@ -1,15 +1,15 @@
 // import validator class
-const joi = require('joi');
+const joi = require("joi");
 // import json web token library
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // import json web token secret
-const formidable = require('formidable');
-const fileConfig = require('../config/fileConfig');
-const { secret } = require('../config');
+const formidable = require("formidable");
+const fileConfig = require("../config/fileConfig");
+const { secret } = require("../config");
 // import response class
-const response = require('../services/responseService');
+const response = require("../services/responseService");
 // import permission class
-const permission = require('../services/accessMapper');
+const permission = require("../services/accessMapper");
 
 // import formidable
 
@@ -17,11 +17,11 @@ const permission = require('../services/accessMapper');
 const getTokenFromHeader = (req) => {
   if (
     (req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Token') ||
+      req.headers.authorization.split(" ")[0] === "Token") ||
     (req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Bearer')
+      req.headers.authorization.split(" ")[0] === "Bearer")
   ) {
-    return req.headers.authorization.split(' ')[1];
+    return req.headers.authorization.split(" ")[1];
   }
   return null;
 };
@@ -39,7 +39,7 @@ module.exports.validateBodyWithToken = (schema, grantedArray) => {
 
       // verify token and check the expiration time.
       const decoded = jwt.verify(getTokenFromHeader(req), secret);
-      permission.validity(decoded.role, granted_array);
+      permission.validity(decoded.role, grantedArray);
       next();
     } catch (error) {
       return response.customError(error, res);
@@ -48,7 +48,7 @@ module.exports.validateBodyWithToken = (schema, grantedArray) => {
     // verify token and check the expiration time.
     jwt.verify(getTokenFromHeader(req), secret, async (err, decoded) => {
       if (err) {
-        return response.customError('Invalid Token', res);
+        return response.customError("Invalid Token", res);
       }
       try {
         const output = await permission.validity(decoded.role, grantedArray);
@@ -58,7 +58,7 @@ module.exports.validateBodyWithToken = (schema, grantedArray) => {
         return response.customError(error, res);
       }
     });
-    return result;
+    // return result;
   };
 };
 
@@ -70,34 +70,33 @@ module.exports.validateBodyWithToken = (schema, grantedArray) => {
 module.exports.validateQueryParameters = (schema) => {
   return (req, res, next) => {
     try {
-      // Validate the API request's query parameters according to the schema defined
-      joi.validate(req.query, schema);
+      const result = joi.validate(req.query, schema);
+      if (result.error) {
+        return response.customError(result.error.details[0].message, res);
+      }
       next();
-    } catch {
-      return response.customError(result.error.details[0].message, res);
+    } catch (err) {
+      return response.customError(err, res);
     }
-    next();
-    return result;
   };
 };
 
 /**
  * validate the API request body according to the schema defined
  * @returns validation Status
- * @param {*} schema
+ @param {} schema
  */
-module.exports.validateBody = (schema) => {
+module.exports.validateBody = function (schema) {
   return (req, res, next) => {
-    try {
-      // validate the API request body according to the schema defined
-      joi.validate(req.body, schema);
-    } catch {
+    // validate the API request body according to the schema defined
+    const result = joi.validate(req.body, schema);
+    if (result.error) {
       return response.customError(result.error.details[0].message, res);
     }
     next();
-    return result;
   };
 };
+
 /**
  * validate the API request header
  * @returns validation Status
@@ -108,7 +107,7 @@ module.exports.validateHeader = (grantedArray) => {
     // eslint-disable-next-line consistent-return
     return jwt.verify(getTokenFromHeader(req), secret, async (err, decoded) => {
       if (err) {
-        return response.customError('Invalid Token', res);
+        return response.customError("Invalid Token", res);
       }
       try {
         await permission.validity(decoded.role, grantedArray);
@@ -117,7 +116,6 @@ module.exports.validateHeader = (grantedArray) => {
         return response.customError(error, res);
       }
     });
-
   };
 };
 
@@ -127,41 +125,36 @@ module.exports.validateHeader = (grantedArray) => {
  * @param {*} schema
  */
 module.exports.validateFormData = (schema) => async (req, res, next) => {
-  const form = formidable({
-    maxFileSize: fileConfig.maxFileSize,
-  });
-  const formFields = await new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        reject(err);
-        return response.customError(`${err}`, res);
+  const form = formidable({ multiples: true });
+  const data = [];
+
+  form.parse(req, (err, fields, files) => {
+    if (err) return response.customError(err);
+
+    if (Array.isArray(files.files)) {
+      for (file of files.files) {
+        data.push({
+          size: file.size,
+          path: file.path,
+          name: file.name,
+          type: file.type,
+        });
       }
-
-      resolve({ fields, files });
-      return null;
-    });
-  });
-
-  const data = {
-    ...formFields.fields,
-    ...formFields.files,
-  };
-
-  Object.entries(data).forEach((entry) => {
-    const key = entry[0];
-    try {
-      data[key] = JSON.parse(data[key]);
-    } catch (error) {
-      // continue regardless of error
+    } else {
+      data.push({
+        size: files.size,
+        path: files.path,
+        name: files.name,
+        type: files.type,
+      });
     }
   });
 
-  const result = schema.validate(data);
-
+  const result = schema.validate({ files: data });
   if (result.error) {
     return response.customError(result.error.details[0].message, res);
   }
-  req.body = data;
+
   next();
   return null;
 };
