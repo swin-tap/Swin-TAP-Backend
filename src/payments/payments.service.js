81be77e90fc727ae321b83b4b58c45767973d274
kmkmadusanka <kmkasunmadushanka@gmail.com>
2024-09-24 20:41:47 +1000

    update stripe with payment link

--- src/payments/payments.service.js
+++ src/payments/payments.service.js
@@ -1,10 +1,12 @@
 // import repository
-const Stripe = require('stripe');
-const repository = require('./payments.repository');
+const Stripe = require("stripe");
+const repository = require("./payments.repository");
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 // import mail service
-const mailSender = require('../../mailHub/miler');
+const mailSender = require("../../mailHub/miler");
+// import payment status
+const { paid } = require("../../config/paymentConfig").status;
 
 /**
  * GET all data set
@@ -36,7 +38,7 @@ module.exports.getById = async (id) => {
     try {
       const data = await repository.findById({ _id: id });
       if (!data || data.length == 0) {
-        reject('No data found from given id');
+        reject("No data found from given id");
       } else {
         resolve(data);
       }
@@ -54,28 +56,36 @@ module.exports.getById = async (id) => {
 module.exports.save = async (obj) => {
   return new Promise(async (resolve, reject) => {
     try {
-      // get client secret from Stripe.
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: obj.amount,
+      // Create a product
+      const product = await stripe.products.create({
+        name: "Vehicle Inspection Fee with AutoAssure.me",
+      });
+
+      // Create a price for the product
+      const price = await stripe.prices.create({
+        unit_amount: obj.amount * 100,
         currency: obj.currency,
+        product: product.id, // above created
       });
 
       // save to DB
       const data = await repository.save(obj);
 
-      const { payment_email, inspection_report, currency, amount } = obj;
-      // send email about payment
-      await mailSender.paymentForInspection(
-        payment_email,
-        inspection_report,
-        currency,
-        amount
-      );
+      // Create a payment link with the price ID
+      const paymentLink = await stripe.paymentLinks.create({
+        line_items: [
+          {
+            price: price.id,
+            quantity: 1,
+          },
+        ],
+        metadata: { productId: data._id.toString() },
+      });
 
       // return client secret to front end.
       resolve({
-        payment_id: data._id,
-        client_secret: paymentIntent.client_secret,
+        productId: data._id,
+        url: paymentLink.url,
       });
     } catch (error) {
       reject(error);
@@ -95,9 +105,55 @@ module.exports.updateSingleObj = async (obj) => {
     try {
       const data = await repository.updateSingleObject({ _id: id }, obj);
       if (!data) {
-        reject('No data found from given id');
+        reject("No data found from given id");
       } else {
         resolve(data);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+/**
+ * Webhook update object
+ * @input {objId}
+ * @output {object}
+ */
+module.exports.webhookUpdate = async (header, obj) => {
+  return new Promise(async (resolve, reject) => {
+    const id = obj._id;
+    delete obj._id;
+    const sig = header["stripe-signature"];
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET;
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(obj, sig, endpointSecret);
+
+      if (event.type === "payment_intent.succeeded") {
+        const paymentIntent = event.data.object;
+        const productId = paymentIntent.metadata.productId;
+        const data = await repository.updateSingleObject(
+          { _id: productId },
+          { status: paid }
+        );
+        if (!data) {
+          reject("No data found from given id");
+        } else {
+          await stripe.products.del(productId);
+          // send email about payment
+          const { payment_email, inspection_report, currency, amount } = data;
+          // send email with successful payment
+          await mailSender.paymentForInspection(
+            payment_email,
+            inspection_report,
+            currency,
+            amount
+          );
+          resolve(data);
+        }
+      } else {
+        reject("Invalid event type");
       }
     } catch (error) {
       reject(error);
@@ -115,7 +171,7 @@ module.exports.DeleteSingleObject = async (id) => {
     try {
       const data = await repository.removeObject({ _id: id });
       if (!data) {
-        reject('No data found from given id');
+        reject("No data found from given id");
       } else {
         resolve(data);
       }
